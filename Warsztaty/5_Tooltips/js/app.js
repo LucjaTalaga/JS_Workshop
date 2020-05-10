2457262138da4dd3f7c68d9a5526e70dab3d03f9
Piotr Talaga <piotrek.talaga@gmail.com>
2020-05-10 18:06:29 +0200

    task 5&6

--- Warsztaty/5_Tooltips/js/app.js
+++ Warsztaty/5_Tooltips/js/app.js
@@ -1,3 +1,20 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", function() {
+
+    const  toolTip = document.querySelectorAll('.tooltip');
+
+    let toolTipText = document.createElement('span');
+    toolTipText.classList.add("tooltipText");
+
+    for (let i = 0; i < toolTip.length; i++) {
+        toolTip[i].addEventListener("mouseover", function(){        
+            toolTipText.innerText = this.dataset.text;
+            this.appendChild(toolTipText);
+        });
+        toolTip[i].addEventListener("mouseout", function(){
+            this.removeChild(toolTipText);
+        });
+    }
+});
--- Warsztaty/6_ToDoList/js/app.js
+++ Warsztaty/6_ToDoList/js/app.js
@@ -1,1 +1,39 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+
+    const taskInput = document.querySelector("#taskInput");
+    const taskList = document.querySelector("#taskList");
+    const addTaskButton = document.querySelector("#addTaskButton");
+    const removeButton = document.querySelector("#removeFinishedTasksButton");
+
+
+
+    addTaskButton.addEventListener("click", function(){
+        if(taskInput.value.length >= 5 && taskInput.value.length <= 100){
+            addTask(taskInput.value);
+        }
+        taskInput.value = null;
+    });
+
+    removeButton.addEventListener("click", function(){
+        let tasks = taskList.children;
+        console.log(tasks);
+        while(taskList.lastChild){
+            taskList.removeChild(taskList.firstChild);
+        }
+    });
+
+    function addTask(task) {
+        let newTask = document.createElement('li');
+        let taskText = document.createElement('h1');
+        taskText.innerText = task;
+        let deleteButton = document.createElement('button');
+        let completeButton = document.createElement('button');
+        deleteButton.innerText = "Delete";
+        completeButton.innerText = "Complete";
+        newTask.appendChild(taskText);
+        newTask.appendChild(deleteButton);
+        newTask.appendChild(completeButton);
+        taskList.appendChild(newTask);
+    }
+});
